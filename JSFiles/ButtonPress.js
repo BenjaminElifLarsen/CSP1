8055b0238e6f931788845da9c541016bce1eb5c2
Benjamin Larsen <beela93@hotmail.com>
2021-01-28 09:25:47 +0100

    base stuff

--- JSFiles/ButtonPress.js
+++ JSFiles/ButtonPress.js
@@ -1,3 +1,18 @@
-﻿function ButtonPress() {
-    alert("Jaaaaaaa");
+﻿var numberOfButtonPresses = 0;
+
+//function ButtonPress() {
+//    numberOfButtonPresses++;
+//    alert("Jaaaaaaa " + numberOfButtonPresses);
+//}
+
+//function ButtonPress(parameter) {
+//    parameter++;
+//    alert("Naaaaaaa " + parameter);
+//    return parameter;
+//}
+
+function ButtonPress(parameter) {
+    alert("Naaaaaaa " + ++parameter);
+    return parameter;
 }
+
